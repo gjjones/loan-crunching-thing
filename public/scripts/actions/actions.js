ec141625e9b13ca7c9381c62c933b2514855bbd0
Greg Jones <gjjones@outlook.com>
2015-10-24 21:59:44 -0400

    Whitespace corrections. Adds editorconfig where I think we're breaking a line# rule. Just a heads up.

--- public/scripts/actions/actions.js
+++ public/scripts/actions/actions.js
@@ -2,15 +2,15 @@ export const ADD_LOAN = "ADD_LOAN";
 export const ADD_LOANS = "ADD_LOANS";
 
 export function addLoan(loan) {
-	return {
-		type: ADD_LOAN,
-		loan
-	}	
+  return {
+    type: ADD_LOAN,
+    loan
+  }
 }
 
 export function addLoans(loans) {
-	return {
-		type: ADD_LOANS,
-		loans
-	}
+  return {
+    type: ADD_LOANS,
+    loans
+  }
 }
--- public/scripts/utils/loan.js
+++ public/scripts/utils/loan.js
@@ -25,18 +25,19 @@ function expandAsAmountPaidByMonth(loan) {
   var amountByMonth = [];
   amountByMonth.length = Math.ceil(loan.term);
   return amountByMonth.fill(undefined)
-      .map(function(_, index) {
-        index++; // needs to be 1-based for mathy stuff
-        var paymentsMade = (loan.term > index) ? index : loan.term;
-        return {
-          monthlyPayment: loan.monthlyPayment,
-          paymentsMade: paymentsMade
-        }
-      })
-      .map(function (paymentsInfo) {
-        return paymentsInfo.paymentsMade * paymentsInfo.monthlyPayment;
-      });
+    .map(function(_, index) {
+      index++; // needs to be 1-based for mathy stuff
+      var paymentsMade = (loan.term > index) ? index : loan.term;
+      return {
+        monthlyPayment: loan.monthlyPayment,
+        paymentsMade: paymentsMade
+      }
+    })
+    .map(function (paymentsInfo) {
+      return paymentsInfo.paymentsMade * paymentsInfo.monthlyPayment;
+    });
 }
+
 module.exports = {
   totalPrincipal,
   paymentsRemaining,
