2ada8c249c93bc0fbb05a221223675be947db9a9
gjjones <gjjones@ebsco.com>
2015-12-09 09:17:32 -0500

    Shuffle around a bunch of component structure, makes better use of components as functions.

--- public/scripts/components/LoanItem.js
+++ public/scripts/components/LoanItem.js
@@ -2,32 +2,28 @@
 import React, { Component } from 'react';
 /*eslint-enable no-unused-vars*/
 
-const NumberField = ({label, value, onChange}) => {
-  return (
-    <label>
-      {label}
-      {' '}
-      <input
-        type='number'
-        value={value}
-        onChange={onChange}
-      />
-    </label>
-  );
-};
-const RadioField = ({label, checked, onChange}) => {
-  return (
-    <label>
-      <input
-        type='radio'
-        checked={checked}
-        onChange={onChange}
-      />
-      {' '}
-      {label}
-    </label>
-  );
-};
+const NumberField = ({label, value, onChange}) => (
+  <label>
+    {label}
+    {' '}
+    <input
+      type='number'
+      value={value}
+      onChange={onChange}
+    />
+  </label>
+)
+const RadioField = ({label, checked, onChange}) => (
+  <label>
+    <input
+      type='radio'
+      checked={checked}
+      onChange={onChange}
+    />
+    {' '}
+    {label}
+  </label>
+)
 
 const EditableNumberLabelField = ({label, value, editing, onChange}) => {
   if (!editing) {
@@ -46,72 +42,54 @@ const EditableNumberLabelField = ({label, value, editing, onChange}) => {
   );
 };
 
-class LoanItem extends Component {
-  render () {
-    let { loan } = this.props;
-    return (
-      <div style={{float: 'left', width: '50%'}}>
-        <h3>I am a loan</h3>
-        <NumberField
-          label='loan principal:'
-          value={loan.get('principal')}
-          onChange={this.handlePrincipalChanged.bind(this)}
-        />
-        <br/>
-        <NumberField
-          label='loan interest rate:'
-          value={loan.get('interestRate')}
-          onChange={this.handleInterestRateChanged.bind(this)}
-        />
-        <br/>
-        <RadioField
-          label='monthly payment'
-          checked={!loan.get('calculatedTerm')}
-          onChange={
-            this.props.onToggleCalculatedField.bind(undefined, loan.get('id'))
-          }
-        />
-        <br/>
-        <RadioField
-          label='term'
-          checked={loan.get('calculatedTerm')}
-          onChange={
-            this.props.onToggleCalculatedField.bind(undefined, loan.get('id'))
-          }
-        />
-        <br/>
-        <EditableNumberLabelField
-          label='loan monthly payment:'
-          value={loan.get('monthlyPayment')}
-          editing={loan.get('calculatedTerm')}
-          onChange={this.handleMonthlyPaymentChanged.bind(this)}
-        />
-        <br/>
-        <EditableNumberLabelField
-          label='loan term:'
-          value={loan.get('term')}
-          editing={!loan.get('calculatedTerm')}
-          onChange={this.handleTermChanged.bind(this)}
-        />
-      </div>
-    );
-  }
-
-  handlePrincipalChanged (event) {
-    this.props.onUpdatePrincipal(this.props.loan.get('id'), event);
-  }
-
-  handleInterestRateChanged (event) {
-    this.props.onUpdateInterestRate(this.props.loan.get('id'), event);
-  }
-
-  handleMonthlyPaymentChanged (event) {
-    this.props.onUpdateMonthlyPayment(this.props.loan.get('id'), event);
-  }
-
-  handleTermChanged (event) {
-    this.props.onUpdateTerm(this.props.loan.get('id'), event);
-  }
-}
+const LoanItem = ({
+  loan,
+  onUpdatePrincipal,
+  onUpdateInterestRate,
+  onToggleCalculatedField,
+  onUpdateMonthlyPayment,
+  onUpdateTerm
+}) => (
+  <div style={{float: 'left', width: '50%'}}>
+    <h3>I am a loan</h3>
+    <NumberField
+      label='loan principal:'
+      value={loan.get('principal')}
+      onChange={onUpdatePrincipal}
+    />
+    <br/>
+    <NumberField
+      label='loan interest rate:'
+      value={loan.get('interestRate')}
+      onChange={onUpdateInterestRate}
+    />
+    <br/>
+    <RadioField
+      label='monthly payment'
+      checked={!loan.get('calculatedTerm')}
+      onChange={onToggleCalculatedField}
+    />
+    <br/>
+    <RadioField
+      label='term'
+      checked={loan.get('calculatedTerm')}
+      onChange={onToggleCalculatedField}
+    />
+    <br/>
+    <EditableNumberLabelField
+      label='loan monthly payment:'
+      value={loan.get('monthlyPayment')}
+      editing={loan.get('calculatedTerm')}
+      onChange={onUpdateMonthlyPayment}
+    />
+    <br/>
+    <EditableNumberLabelField
+      label='loan term:'
+      value={loan.get('term')}
+      editing={!loan.get('calculatedTerm')}
+      onChange={onUpdateTerm}
+    />
+  </div>
+)
 
 export default LoanItem;
--- public/scripts/components/LoanList.js
+++ public/scripts/components/LoanList.js
@@ -12,54 +12,64 @@ import {
   updateTerm
 } from '../actions/actions';
 
-class LoanList extends Component {
-  render () {
-    let loansMarkup = this.props.loans.map(function (loan) {
-      return (
-        <LoanItem
-          key={loan.get('id')}
-          loan={loan}
-          onToggleCalculatedField={this.toggleCalculatedField.bind(this)}
-          onUpdatePrincipal={this.updatePrincipal.bind(this)}
-          onUpdateInterestRate={this.updateInterestRate.bind(this)}
-          onUpdateMonthlyPayment={this.updateMonthlyPayment.bind(this)}
-          onUpdateTerm={this.updateTerm.bind(this)}
-        />
-      );
-    }.bind(this));
-    return (
-      <div>
-        <h2>I am loan info!</h2>
-        {loansMarkup}
-      </div>
-    );
-  }
+var LoanList = ({
+  loans,
+  onLoanToggleCalculatedField,
+  onLoanUpdatePrincipal,
+  onLoanUpdateInterestRate,
+  onLoanUpdateMonthlyPayment,
+  onLoanUpdateTerm
+}) => (
+  <div>
+    <h2>I am loan info!</h2>
+    {loans.map(loan =>
+      <LoanItem
+        key={loan.get('id')}
+        loan={loan}
+        onToggleCalculatedField={() =>
+          onLoanToggleCalculatedField(loan.get('id'))
+        }
+        onUpdatePrincipal={(event) =>
+          onLoanUpdatePrincipal(loan.get('id'), event.target.value)
+        }
+        onUpdateInterestRate={(event) =>
+          onLoanUpdateInterestRate(loan.get('id'), event.target.value)
+        }
+        onUpdateMonthlyPayment={(event) =>
+          onLoanUpdateMonthlyPayment(loan.get('id'), event.target.value)
+        }
+        onUpdateTerm={(event) =>
+          onLoanUpdateTerm(loan.get('id'), event.target.value)
+        }
+      />
+    )}
+  </div>
+)
 
-  toggleCalculatedField (loanId) {
-    this.props.dispatch(toggleCalculatedField(loanId));
-  }
-
-  updatePrincipal (loanId, event) {
-    this.props.dispatch(updatePrincipal(loanId, event.target.value));
-  }
-
-  updateInterestRate (loanId, event) {
-    this.props.dispatch(updateInterestRate(loanId, event.target.value));
-  }
-
-  updateMonthlyPayment (loanId, event) {
-    this.props.dispatch(updateMonthlyPayment(loanId, event.target.value));
-  }
-
-  updateTerm (loanId, event) {
-    this.props.dispatch(updateTerm(loanId, event.target.value));
-  }
-}
 
 function mapStateToProps (state) {
   return {
     loans: state.loans
   };
 }
+function mapDispatchToProps (dispatch) {
+  return {
+    onLoanToggleCalculatedField: (loanId) => {
+      dispatch(toggleCalculatedField(loanId))
+    },
+    onLoanUpdatePrincipal: (loanId, value) => {
+      dispatch(updatePrincipal(loanId, value))
+    },
+    onLoanUpdateInterestRate: (loanId, value) => {
+      dispatch(updateInterestRate(loanId, value))
+    },
+    onLoanUpdateMonthlyPayment: (loanId, value) => {
+      dispatch(updateMonthlyPayment(loanId, value))
+    },
+    onLoanUpdateTerm: (loanId, value) => {
+      dispatch(updateTerm(loanId, value))
+    }
+  };
+}
 
-export default connect(mapStateToProps)(LoanList);
+export default connect(mapStateToProps, mapDispatchToProps)(LoanList);
